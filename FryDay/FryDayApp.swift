3f51a00616b816fd928d7cfb6523388ee9156320
Theo <theo.goodman@gmail.com>
2023-01-17 14:29:08 -0500

    Initial Commit

---
+++ FryDay/FryDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FryDayApp.swift
+//  FryDay
+//
+//  Created by Theo Goodman on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct FryDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
